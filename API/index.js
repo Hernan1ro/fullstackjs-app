bb63e2a5ce1ce3d902b6a84f9952f78fb3d63804
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-18 18:40:51 -0500

    downgrade the dependencies to fix deprecated bungs

--- API/index.js
+++ API/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 // Crear el servidor
 const app = express();
+
+// Conectar a mongoDB
+mongoose.Promise = global.Promise;
+
+mongoose.connect("mongodb://localhost/veterinaria", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 // Definir el puerto
 
