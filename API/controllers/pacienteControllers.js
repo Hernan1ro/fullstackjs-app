26e18fde1a52b48a8f1375e0dfbb10112db1eadf
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-19 14:06:14 -0500

    fixing mongoDb connection

--- API/controllers/pacienteControllers.js
+++ API/controllers/pacienteControllers.js
@@ -1,9 +1,17 @@
+const Paciente = require("../models/Paciente.js");
+
 //Cuando se crea un nuevo client
 
-exports.nuevoCliente = (req, res, next) => {
+exports.nuevoCliente = async (req, res, next) => {
+  // Crear objeto de paciente con datos de req.body
+  const paciente = new Paciente(req.body);
   //TODO: Insertar en la base de datos
-
-  console.log(req.body);
-
-  res.json({ mensaje: "El cliente se agregó correctamente" });
+  try {
+    const resultado = await paciente.save();
+    res.json({ mensaje: "El cliente se agregó correctamente" });
+    console.log(resultado);
+  } catch (err) {
+    console.log("No se pudo agregar al paciente " + err);
+    next();
+  }
 };
--- API/index.js
+++ API/index.js
@@ -5,14 +5,22 @@ const bodyParser = require("body-parser");
 // Crear el servidor
 const app = express();
 
-// Conectar a mongoDB
-mongoose.Promise = global.Promise;
+const url =
+  "mongodb+srv://hernan1ro:techlover@veterinaria.mtzfx.mongodb.net/veterinaria?retryWrites=true&w=majority";
 
-mongoose.connect("mongodb://localhost/veterinaria", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+// Conectar a mongoDB
+// mongoose.Promise = global.Promise;
+mongoose
+  .connect(url, (err) => {
+    if (!err) {
+      console.log("db conected");
+    } else {
+      console.log("db error");
+    }
+  })
+  .then(() => console.log("Base DB conectada"))
+  .catch((err) => console.log("Ha habido un error ", err));
+
 // Habilitar el body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,6 +30,6 @@ app.use("/", routes());
 
 // Definir el puerto
 
-app.listen(4000, () => {
-  console.log("Servidor funcionando");
+app.listen(27017, () => {
+  console.log("Servidor funcionando en el puerto " + 27017);
 });
