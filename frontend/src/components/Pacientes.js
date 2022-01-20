1831a60cc7266343c4fe17bf54dfebbedf08cf07
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-20 09:57:39 -0500

    creating a form to create new appointment

--- frontend/src/components/Pacientes.js
+++ frontend/src/components/Pacientes.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Pacientes = ({ citas }) => {
   if (citas.length === 0) return null;
@@ -8,12 +9,13 @@ const Pacientes = ({ citas }) => {
       <div className="container mt-5 py-5">
         <div className="row">
           <div className="col-12 mb-5 d-flex justify-content-center">
-            <a
+            <Link
+              to="/nuevacita"
               href="#"
               className="btn btn-success text-uppercase py-2 px-5 font-weight-bold"
             >
               Crear cita
-            </a>
+            </Link>
           </div>
           <div className="col-md-8 mx-auto">
             <div className="list-group">
