4ee04a0357b09af5a7220f7a485585fe6b45e2f9
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-20 16:10:14 -0500

    showing HTML in electron

--- Desktop/js/app.js
+++ Desktop/js/app.js
@@ -1,4 +1,24 @@
 fetch("http://localhost:27017/pacientes")
   .then((res) => res.json())
-  .then((data) => console.log(data))
+  .then((data) => mostrarCitas(data))
   .catch((err) => console.log(err));
+
+function mostrarCitas(citas) {
+  const contenedorCitas = document.querySelector("#citas");
+
+  citas.forEach((cita) => {
+    contenedorCitas.innerHTML += `<div class="p-5 list-group-item list-group-item-action flex-column align-items-start">
+        <div class="d-flex w-100 justify-content-between mb-4">
+          <h3 class="mb-3">${cita.nombre}</h3>
+          <small class="fecha-alta">
+            ${cita.fecha} - ${cita.hora}
+          </small>
+        </div>
+        <p class="mb-0">${cita.sintomas}</p>
+        <div class="contacto py-3">
+          <p>Dueño: ${cita.propietario}</p>
+          <p>Telefono: ${cita.telefono}</p>
+        </div>
+      </div>`;
+  });
+}
