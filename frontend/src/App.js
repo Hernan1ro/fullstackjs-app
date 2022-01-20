65a87de17e3a441202261d02eed7a08b0550cb54
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-20 08:47:43 -0500

    Querying the backend with axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate, Routes, Route, BrowserRouter } from "react-router-dom";
+import clienteAxios from "./config/axios";
 import Pacientes from "./components/Pacientes";
 import NuevaCita from "./components/NuevaCita";
 import Cita from "./components/Cita";
 
 function App() {
+  // State de la aplicación
+  const [citas, setCitas] = useState([]);
+  useEffect(() => {
+    const consultarAPI = () => {
+      clienteAxios
+        .get("/pacientes")
+        .then((res) => console.log(res))
+        .catch((err) => console.error(err));
+    };
+    consultarAPI();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
