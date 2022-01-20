348e5bba9d2478b184dfd06f08cade9ade3350fa
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-20 00:07:05 -0500

    stablishing a routing for the pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Navigate, Routes, Route, BrowserRouter } from "react-router-dom";
+import Pacientes from "./components/Pacientes";
+import NuevaCita from "./components/NuevaCita";
+import Cita from "./components/Cita";
 
 function App() {
-  return <h1>Pacientes</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/pacientes" element={<Pacientes />} />
+        <Route path="/nuevacita" element={<NuevaCita />} />
+        <Route path="/cita/:id" element={<Cita />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Pacientes.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pacientes = () => {
+  return <h1>Desde pacientes</h1>;
+};
+
+export default Pacientes;
