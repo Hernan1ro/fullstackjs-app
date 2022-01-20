7d6c55dffc3d47351a864bcaa569fe7f86a9e6fb
Hernán D. Mercado <hernandmf@gmail.com>
2022-01-20 18:17:42 -0500

    finishing the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Navigate, Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import clienteAxios from "./config/axios";
 import Pacientes from "./components/Pacientes";
 import NuevaCita from "./components/NuevaCita";
